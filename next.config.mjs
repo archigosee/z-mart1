a60e666c565520ace164db9f4ed3a76fc4041cb6
Dorian <kaleabmekonnen45@gmail.com>
2024-08-19 08:44:56 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        API_URL:"https://z-mart1.vercel.app/",
+        API_URL:"https://z-mart1.vercel.app",
         DB_URI:"mongodb+srv://kaleab:%40Rch1g0s@new.rtiisbo.mongodb.net/?retryWrites=true&w=majority&appName=new"
     },
     images:{
