9d780d48390c3ac202ac1699e54054d1f88cf3c6
Dorian <kaleabmekonnen45@gmail.com>
2024-09-09 11:52:21 +0300

    next

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output:"standalone",
     env:{
         API_URL:"http://localhost:3000",
         DB_URI:"mongodb+srv://kaleabnew21:kaleab1221@mart.tywhp.mongodb.net/?retryWrites=true&w=majority&appName=Mart"
