0edbb8e6fa1edf12802910a3d053db2995b43dc6
Dorian <kaleabmekonnen45@gmail.com>
2024-09-09 15:24:28 +0300

    env

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output:"standalone",
     env:{
-        API_URL:"http://localhost:3000",
+        API_URL:"https://z-mart1.vercel.app",
         DB_URI:"mongodb+srv://kaleabnew21:kaleab1221@mart.tywhp.mongodb.net/?retryWrites=true&w=majority&appName=Mart"
     },
     images:{
