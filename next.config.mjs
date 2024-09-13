68b813db84539c855823f98e2e93c468431a8778
Dorian <kaleabmekonnen45@gmail.com>
2024-09-13 08:43:56 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"standalone",
-    env:{
-        API_URL:"http://localhost:3000",
-        DB_URI:"mongodb+srv://kaleabnew21:kaleab1221@mart.tywhp.mongodb.net/?retryWrites=true&w=majority&appName=Mart"
+    output: "standalone",
+    env: {
+        API_URL: "https://wagamart.azurewebsites.net",
+        DB_URI: "mongodb+srv://kaleabnew21:kaleab1221@mart.tywhp.mongodb.net/?retryWrites=true&w=majority&appName=Mart"
     },
-    images:{
-        domains:['res.cloudinary.com']
+    images: {
+        domains: ['res.cloudinary.com']
     }
 };
 
