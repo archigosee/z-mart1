cc1f12b6307320f9294dbd76fb8d4fa5c0d84bc9
Dorian <kaleabmekonnen45@gmail.com>
2024-09-13 13:43:44 +0300

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
 
     env: {
-        API_URL: "https://waga.azurewebsites.net",
+        API_URL: "https://wagamart.azurewebsites.net/",
         DB_URI: "mongodb+srv://kaleabnew21:kaleab1221@mart.tywhp.mongodb.net/?retryWrites=true&w=majority&appName=Mart"
     },
     images: {
