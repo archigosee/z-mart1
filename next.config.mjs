e9e47fc27e1c1371fceb92aa9fc8f01c3abcbd08
Dorian <kaleabmekonnen45@gmail.com>
2024-09-17 15:21:27 +0300

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
 
     env: {
         API_URL: "https://wagamart.azurewebsites.net/",
-        DB_URI: "mongodb://wagadb:E4ioEaEnRfY1FgJ9uLWefww5HF3IRftijqNYpkCZkz2PCXIbznGNZecpy299FHSGyjR3WouvoEUgACDbPfNlrw==@wagadb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@wagadb@"
+        DB_URI: process.env.DB_URI
     },
     images: {
         domains: ['res.cloudinary.com']
